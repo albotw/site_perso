e3e9d073a96a623cb18699dc0e0d2cdd273711b2
albotw <16798879+albotw@users.noreply.github.com>
2021-05-13 20:43:08 +0200

    préparation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" src="CSS/base.css" type="text/css"/>
+    <title>Yann Trou &ndash; Accueil</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
